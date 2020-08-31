96d70579b3eb3dd1fdfda4ee02783b9ed5c365e1
Matthew <Mattpet26@gmail.com>
2020-08-31 15:52:40 -0700

    added login and landing page

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 'use strict';
 
-//packages
-
+//===================================================== Packages ===================================================================
 require('dotenv').config();
 const express = require('express');
 const pg = require('pg');
@@ -9,7 +8,7 @@ const superagent = require('superagent');
 const methodOverride = require('method-override');
 const app = express();
 
-// global variables
+//===================================================== Global Vars =================================================================
 const PORT = process.env.PORT || 3001;
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
@@ -19,16 +18,37 @@ app.use(methodOverride('_method'));
 const client = new pg.Client(process.env.DATABASE_URL);
 client.on('error', console.error);
 
-//express configs
+//===================================================== Routes =====================================================================
+app.get('/', getUserName)
+app.post('/user', insertUserFromSQL)
+app.get('/index', renderHomePage)
+
+//===================================================== Functions ==================================================================
+function getUserName(req, res){
+  const SQL = 'SELECT * FROM user_table;'
+  client.query(SQL)
+    .then(result =>{
+      res.render('pages/login', {users : result.rows})
+    })
+  }
+
+function insertUserFromSQL(req, res){
+  const SQL = `INSERT INTO user_table (username) VALUES ($1)`
+  const value = [req.body.username]
+  client.query(SQL, value)
+    .then(result =>{
+      res.redirect('/index')
+    })
+}
+
+function renderHomePage(req, res){
+  res.render('pages/index')
+}
+
+//===================================================== Constructor ================================================================
 
 
-//routes
-
-
-//constructors
-
-
-//start app
+//===================================================== Start Server ===============================================================
 client.connect()
 .then(() => {
   app.listen(PORT, () => console.log(`This is running the server on PORT : ${PORT} working`))
