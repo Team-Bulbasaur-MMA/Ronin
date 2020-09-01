ab1ff1e0064fcce687eee2bacd07e2ecad7e53fb
Marchael Acode <m.acode@outlook.com>
2020-09-01 10:52:42 -0700

    Fixing the functionality of google map and styling

--- public/js/app.js
+++ public/js/app.js
@@ -1,26 +1,57 @@
 function initMap() {
-  const myLatlng = { lat: -25.363, lng: 131.044 };
-  const myLatlng1 = { lat: -25.101, lng: 131.044 };
-  const map = new google.maps.Map(document.getElementById("map"), {
-    zoom: 4,
-    center: myLatlng
+  const latLng = { lat: 38.9048, lng: 137.2529 };
+  const myLatlng = { lat: 35.6762, lng: 139.6503 };
+  const myLatlng1 = { lat: 34.6413, lng: 135.5629 };
+  const myLatlng2 = { lat: 35.0116, lng: 135.5629 };
+  const myLatlng3 = { lat: 34.3853, lng: 132.4553 };
+  const myLatlng4 = { lat: 43.0618, lng: 141.3545 };
+  const map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 6,
+    center: latLng
   });
   const marker = new google.maps.Marker({
     position: myLatlng,
     map,
-    title: "Click to zoom"
+    title: 'Click to zoom'
   });
   const marker1 = new google.maps.Marker({
     position: myLatlng1,
     map,
-    title: "Click to zoom"
+    title: 'Click to zoom'
   });
-  marker.addListener("click", () => {
-    map.setZoom(8);
+  const marker2 = new google.maps.Marker({
+    position: myLatlng2,
+    map,
+    title: 'Click to zoom'
+  });
+  const marker3 = new google.maps.Marker({
+    position: myLatlng3,
+    map,
+    title: 'Click to zoom'
+  });
+  const marker4 = new google.maps.Marker({
+    position: myLatlng4,
+    map,
+    title: 'Click to zoom'
+  });
+  marker.addListener('click', () => {
+    map.setZoom(10.5);
     map.setCenter(marker.getPosition());
   });
-  marker1.addListener("click", () => {
-    map.setZoom(8);
+  marker1.addListener('click', () => {
+    map.setZoom(10.5);
     map.setCenter(marker1.getPosition());
+  });
+  marker2.addListener('click', () => {
+    map.setZoom(10.5);
+    map.setCenter(marker2.getPosition());
+  });
+  marker3.addListener('click', () => {
+    map.setZoom(10.5);
+    map.setCenter(marker3.getPosition());
+  });
+  marker4.addListener('click', () => {
+    map.setZoom(10.5);
+    map.setCenter(marker4.getPosition());
   });
 }
